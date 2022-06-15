e99524d30a760715e637653259310b06b0303245
BMR FASHIONS <BMR FASHIONS@BMR-FASHIONS>
2022-06-15 15:52:40 +0530

    Hello World

--- TestJavaProject/src/com/balu/app/HelloWorld.java
+++ TestJavaProject/src/com/balu/app/HelloWorld.java
@@ -2,6 +2,6 @@ package com.balu.app;
 
 public class HelloWorld {
 public static void main(String[] args) {
-	System.out.println("Hello Balu! Welcome to Java World");
+	System.out.println("Hello Bala! Welcome to Java World");
 }
 }
