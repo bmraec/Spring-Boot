7ffb55c3341c38a699d58ebb9f364269d5372cc4
BMR FASHIONS <BMR FASHIONS@BMR-FASHIONS>
2022-06-15 16:05:32 +0530

    Hello Team

--- TestJavaProject/src/com/balu/app/SecondApp.java
+++ TestJavaProject/src/com/balu/app/SecondApp.java
@@ -2,6 +2,6 @@ package com.balu.app;
 
 public class SecondApp {
 public static void main(String[] args) {
-	System.out.println("Hello Guys ! This is second file.");
+	System.out.println("Hello Team ! This is second file.");
 }
 }
